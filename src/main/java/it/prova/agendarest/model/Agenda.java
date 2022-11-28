969571feaf358a1ba5df6882b5eb1405d991475c
Balraj1108 <balrajsingh1108@gmail.com>
2022-11-28 14:41:31 +0100

    Creazione model Agenda

---
+++ src/main/java/it/prova/agendarest/model/Agenda.java
@@ -0,0 +1,18 @@
+package it.prova.agendarest.model;
+
+import java.time.LocalTime;
+
+public class Agenda {
+
+	private Long id;
+	
+	private String descrizione;
+	
+	private LocalTime dataOraInizio;
+	
+	private LocalTime dataOraFine;
+	
+	private Utente utente;
+	
+	
+}
